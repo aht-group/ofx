7a00dbe20cb54e336ace7615d268b97507167d22
thorsten-k <t.kisner@web.de>
2009-02-14 11:19:28 +0000

    ConfigurationFactory added

--- src/org/openfuxml/util/config/factory/ClientConfFactory.java
+++ src/org/openfuxml/util/config/factory/ClientConfFactory.java
@@ -1,15 +1,18 @@
 package org.openfuxml.util.config.factory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import org.apache.log4j.Logger;
-import org.openfuxml.util.config.jaxb.Dirs;
-import org.openfuxml.util.config.jaxb.Files;
-import org.openfuxml.util.config.jaxb.Net;
 import org.openfuxml.util.config.jaxb.Openfuxml;
-import org.openfuxml.util.config.jaxb.Dirs.Dir;
+import org.openfuxml.util.config.jaxb.Openfuxml.Net;
 
 import de.kisner.util.LoggerInit;
 
@@ -24,50 +27,52 @@ public class ClientConfFactory
 		
 	}
 	
+	public void init(File fConf)
+	{
+		try
+		{
+			if(!fConf.exists())
+			{
+				createConfig();
+				FileOutputStream fos = new FileOutputStream(fConf);
+				writeConfig(fos);
+			}
+		}
+		catch (FileNotFoundException e) {logger.error(e);}
+	}
+	
 	private Net getNet()
 	{
 		Net net =  new Net();
-		net.setServer("testN");
+		net.setHost("host");
+		net.setPort(4444);
 		return net;
-	}
-	
-	private Dirs getDirs()
-	{
-		Dirs dirs = new Dirs();
-	
-		Dir dir = new Dir();
-			dir.setType("basedir");
-			dirs.getDir().add(dir);
-		 
-		return dirs;
-	}
-	
-	private Files getFiles()
-	{
-		Files files = new Files();
-		files.setServer("testF");
-		return files;
 	}
 	
 	public void createConfig()
 	{
+		ConfDirFactory cdf = new ConfDirFactory();
+		ConfFileFactory cff = new ConfFileFactory();
+		
 		openfuxml = new Openfuxml();
+		openfuxml.setServer("direct");
 		openfuxml.setNet(getNet());
-		openfuxml.setDirs(getDirs());
-		openfuxml.setFiles(getFiles());
+		openfuxml.setDirs(cdf.getDirs());
+		openfuxml.setFiles(cff.getFiles());
 	}
 	
-	public void writeConfig()
+	public void writeConfig(OutputStream os)
 	{
 		try
 		{
 			JAXBContext context = JAXBContext.newInstance(Openfuxml.class);
 			Marshaller m = context.createMarshaller(); 
 			m.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE ); 
-			m.marshal( openfuxml, System.out);
-			
+			m.marshal( openfuxml, os);
+			os.close();
 		}
 		catch (JAXBException e) {logger.error(e);}
+		catch (IOException e) {logger.error(e);}
 	}
 	
 	public static void main(String args[]) 
@@ -78,6 +83,6 @@ public class ClientConfFactory
 			
 		ClientConfFactory ccf = new ClientConfFactory();
 			ccf.createConfig();
-			ccf.writeConfig();
+			ccf.writeConfig(System.out);
 	}
 }
