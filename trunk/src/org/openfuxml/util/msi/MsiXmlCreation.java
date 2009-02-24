e53690bea63da12398f17240a0768d0974e0e3a8
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2009-02-24 06:09:26 +0000

    msixml now with ANT-Task and in own package

--- trunk/src/org/openfuxml/util/msi/MsiXmlCreation.java
+++ trunk/src/org/openfuxml/util/msi/MsiXmlCreation.java
@@ -1,41 +1,18 @@
-package org.openfuxml.util;
+package org.openfuxml.util.msi;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
-import org.apache.tools.ant.Task;
+import de.kisner.util.LoggerInit;
 
-
-public class AntTaskMsiXmlCreation extends Task {
+public class MsiXmlCreation {
 	
 	public static int               id_counter;
 	public static ArrayList<String> components;
 	public static ArrayList<File>   dir_queue;
 	public static StringBuffer      sb;
-	public static String            root;
-	public static String            output;
-	public static String            filename;
 	public static String            linebreak;
-	
-	public void setRootDir(String rootDir)
-	{
-		root = rootDir;
-	}
-	
-
-	public void setOutputDir(String outputDir)
-	{
-		output = outputDir;
-	}
-	
-	public void setFilename(String fileName)
-	{
-		filename = fileName;
-	}
 	
 	public static void addFiles(File directory)
 	{
@@ -93,7 +70,7 @@ public class AntTaskMsiXmlCreation extends Task {
 		sb.append("<?xml version='1.0' encoding='utf-8'?>" +linebreak);
 		sb.append("<Wix xmlns='http://schemas.microsoft.com/wix/2003/01/wi'>" + linebreak);
 		sb.append("<Product Id='87952548-8B19-4192-B997-05C2C55C9B7D' Name='openFuXML' Language='1033' Version='@@@openfuxml-version@@@' Manufacturer='OpenSource project hostet at SourceForge'>" + linebreak);
-		sb.append("    <Package Id='34BD222D-7C1A-4660-873D-07185FD40E88' Description='Redaktionssystem fÃ¼r professionelle Dokumente in Forschung und Lehre' Comments='Install Package for openFuXML on Windows Machines' InstallerVersion='200' Compressed='yes' />" + linebreak);
+		sb.append("    <Package Id='34BD222D-7C1A-4660-873D-07185FD40E88' Description='Redaktionssystem fŸr professionelle Dokumente in Forschung und Lehre' Comments='Install Package for openFuXML on Windows Machines' InstallerVersion='200' Compressed='yes' />" + linebreak);
 		sb.append("         <Media Id='1' Cabinet='simple.cab' EmbedCab='yes' />" + linebreak);
 		sb.append("         <Directory Id='TARGETDIR' Name='SourceDir'>" + linebreak);
 		sb.append("         <Directory Id='ProgramFilesFolder' Name='PFiles'>" + linebreak);
@@ -113,26 +90,18 @@ public class AntTaskMsiXmlCreation extends Task {
 		sb.append("</Wix>" + linebreak);
 	}
 	
-	public void execute() {
+	public static void main(String[] args)
+	{
 		id_counter=0;
 		components = new ArrayList<String>();
 		dir_queue = new ArrayList<File>();
 		sb = new StringBuffer();
 		linebreak = System.getProperty("line.separator");
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+		loggerInit.addAltPath("resources/config");
 		writeHeader();
-		addDirectory(new File(root));
+		addDirectory(new File(args[0]));
 		writeFooter();
-		FileWriter datei;
-		try {
-			datei = new FileWriter(output +filename);
-			BufferedWriter textstream =new BufferedWriter(datei);
-			textstream.write(sb.toString());
-			textstream.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Written XML File:");
 		System.out.println(sb.toString());
 	}
 }
