c6d8359604f4f3a6694d9adea98a056a783ec5be
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2009-09-14 09:16:15 +0000

    XmlProcessor added

---
+++ trunk/src/org/openfuxml/addon/wiki/processing/XmlProcessor.java
@@ -0,0 +1,38 @@
+package org.openfuxml.addon.wiki.processing;
+
+import net.sf.exlp.util.JDomUtil;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.log4j.Logger;
+import org.jdom.DocType;
+import org.jdom.Document;
+
+public class XmlProcessor
+{
+	static Logger logger = Logger.getLogger(XmlProcessor.class);
+	private Document doc;
+	
+	public XmlProcessor(Configuration config)
+	{
+		
+	}
+	
+	public Document process(String xHtml)
+	{
+		doc = JDomUtil.txtToDoc(xHtml);
+		setDocType(1);
+		return doc;
+	}
+	
+	private void setDocType(int dtdLevel)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<dtdLevel;i++)
+		{
+			sb.append("../");
+		}
+		DocType docType = new DocType("abschnitt");
+		docType.setSystemID(sb.toString()+"system/dtd/fernuni01.dtd");
+		doc.setDocType(docType);
+	}
+}
