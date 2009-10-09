9e6ae7c6b5a1cace179cc2e8150ad781dad779a8
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2009-10-09 13:07:28 +0000

    Factory now cares about log4j and System.out (for ANT)

--- trunk/src/org/openfuxml/addon/jsf/JsfTagTransformator.java
+++ trunk/src/org/openfuxml/addon/jsf/JsfTagTransformator.java
@@ -1,10 +1,7 @@
 package org.openfuxml.addon.jsf;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
