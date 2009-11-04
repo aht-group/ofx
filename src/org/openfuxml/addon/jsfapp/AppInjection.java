f71a9ebba38d5d225f047785c7c522a6cde5bb0b
thorsten-k <t.kisner@web.de>
2009-11-04 20:01:43 +0000

    AppInjection added

---
+++ src/org/openfuxml/addon/jsfapp/AppInjection.java
@@ -0,0 +1,18 @@
+package org.openfuxml.addon.jsfapp;
+
+import java.io.File;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import de.kisner.util.LoggerInit;
+
+public class AppInjection extends Task
+{		
+	public AppInjection()
+	{
+		
+	}
+	
+ 
+}
