6ae545dcb177d008459974f625538ea787c61f4b
thorsten-k <t.kisner@web.de>
2009-11-14 07:15:31 +0000

    Ofxchart now with grid

--- src/org/openfuxml/addon/wiki/util/OfxchartUtil.java
+++ src/org/openfuxml/addon/wiki/util/OfxchartUtil.java
@@ -4,9 +4,11 @@ import org.apache.log4j.Logger;
 import org.openfuxml.addon.wiki.data.jaxb.Ofx;
 import org.openfuxml.addon.wiki.data.jaxb.Ofxchart;
 
-public class OfxchartSelector
+import com.aht.util.translation.Translation;
+
+public class OfxchartUtil
 {
-	static Logger logger = Logger.getLogger(OfxchartSelector.class);
+	static Logger logger = Logger.getLogger(OfxchartUtil.class);
 	
 	public static synchronized Ofxchart getChart(Ofx ofx, String id, String type)
 	{
@@ -16,5 +18,15 @@ public class OfxchartSelector
 		}
 		logger.warn("Chart not found for id="+id+" type="+type);
 		return null;
+	}
+	
+	public static synchronized Ofxchart translate(Ofxchart ofxchart, Translation translation, String lang)
+	{
+		if(ofxchart.isSetXAxis() && ofxchart.getXAxis().isSetKey())
+		{
+			String label = translation.translate(ofxchart.getXAxis().getKey(),lang);
+			ofxchart.getXAxis().setLabel(label);
+		}
+		return ofxchart;
 	}
 }
