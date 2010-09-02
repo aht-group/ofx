3647fe8f4f8a6ca7f19658e27eb0ef85908dfbdb
thorsten-k <t.kisner@web.de>
2010-09-02 09:03:45 +0000

    LoremIpsum added for hello-world

--- src/org/openfuxml/factory/epub/EpubSectionFactory.java
+++ src/org/openfuxml/factory/epub/EpubSectionFactory.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
 import org.jdom.Namespace;
+import org.openfuxml.content.ofx.Paragraph;
 import org.openfuxml.content.ofx.Section;
 import org.openfuxml.content.ofx.Title;
 import org.openfuxml.producer.preprocessors.ExternalMerger;
@@ -34,8 +35,10 @@ public class EpubSectionFactory
 		for(Serializable s : section.getContent())
 		{
 			if(s instanceof Title){result.add(titleFactory.createTitle((Title)s));}
-			if(s instanceof Section){result.addAll(createSection((Section)s, depth++));}
-			logger.debug(s.getClass().getSimpleName());
+			else if(s instanceof Section){result.addAll(createSection((Section)s, depth++));}
+			else if(s instanceof Paragraph){result.add(paragraphFactory.createParagraph((Paragraph)s));}
+			else if(s instanceof String){}
+			else {logger.warn("Unknwon Content: "+s.getClass().getSimpleName());}
 		}		
 		return result;
 	}
