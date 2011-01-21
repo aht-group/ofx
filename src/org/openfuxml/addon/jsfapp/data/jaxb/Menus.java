27b60e769e2ca63c3b8d4711f6d9481d05ea5e84
thorsten-k <t.kisner@web.de>
2011-01-21 18:34:32 +0000

    Added menus-container

--- src/org/openfuxml/addon/jsfapp/data/jaxb/Menus.java
+++ src/org/openfuxml/addon/jsfapp/data/jaxb/Menus.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -29,9 +28,8 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{http://www.openfuxml.org/jsfapp}menuitem" maxOccurs="unbounded"/>
+ *         &lt;element ref="{http://www.openfuxml.org/jsfapp}menu" maxOccurs="unbounded"/>
  *       &lt;/sequence>
- *       &lt;attribute name="code" type="{http://www.w3.org/2001/XMLSchema}string" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -41,82 +39,52 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "menuitem"
+    "menu"
 })
-@XmlRootElement(name = "menu")
-public class Menu
+@XmlRootElement(name = "menus")
+public class Menus
     implements Serializable
 {
 
     private final static long serialVersionUID = 1L;
     @XmlElement(required = true)
-    protected List<Menuitem> menuitem;
-    @XmlAttribute
-    protected String code;
+    protected List<Menu> menu;
 
     /**
-     * Gets the value of the menuitem property.
+     * Gets the value of the menu property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the menuitem property.
+     * This is why there is not a <CODE>set</CODE> method for the menu property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getMenuitem().add(newItem);
+     *    getMenu().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Menuitem }
+     * {@link Menu }
      * 
      * 
      */
-    public List<Menuitem> getMenuitem() {
-        if (menuitem == null) {
-            menuitem = new ArrayList<Menuitem>();
+    public List<Menu> getMenu() {
+        if (menu == null) {
+            menu = new ArrayList<Menu>();
         }
-        return this.menuitem;
+        return this.menu;
     }
 
-    public boolean isSetMenuitem() {
-        return ((this.menuitem!= null)&&(!this.menuitem.isEmpty()));
+    public boolean isSetMenu() {
+        return ((this.menu!= null)&&(!this.menu.isEmpty()));
     }
 
-    public void unsetMenuitem() {
-        this.menuitem = null;
-    }
-
-    /**
-     * Gets the value of the code property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * Sets the value of the code property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setCode(String value) {
-        this.code = value;
-    }
-
-    public boolean isSetCode() {
-        return (this.code!= null);
+    public void unsetMenu() {
+        this.menu = null;
     }
 
 }
