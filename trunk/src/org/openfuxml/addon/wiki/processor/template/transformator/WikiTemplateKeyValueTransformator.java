a23611c53746c999cc33f3385664397361876dd3
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-03-28 04:00:16 +0000

    KeyValue Transformator with model

--- trunk/src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
+++ trunk/src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
@@ -1,9 +1,16 @@
 package org.openfuxml.addon.wiki.processor.template.transformator;
 
+import net.sf.exlp.io.ConfigLoader;
+import net.sf.exlp.io.LoggerInit;
+import net.sf.exlp.util.xml.JDomUtil;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.apache.commons.configuration.Configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
 import org.jdom.Namespace;
+import org.openfuxml.addon.wiki.data.jaxb.Template;
 import org.openfuxml.addon.wiki.processor.util.AbstractWikiProcessor;
 import org.openfuxml.addon.wiki.processor.util.WikiProcessor;
 
@@ -20,9 +27,30 @@ public class WikiTemplateKeyValueTransformator extends AbstractWikiProcessor imp
 	
 	public Element transform(String wikiMarkup)
 	{
+		logger.debug(wikiMarkup);
 		Element e = new Element("paragraph");
 		e.setNamespace(nsOfx);
 		e.setText("This is the content!!");
 		return e;
+	}
+	
+	public static void main (String[] args) throws Exception
+	{
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+			loggerInit.addAltPath("resources/config");
+			loggerInit.init();
+		
+		String propFile = "resources/properties/user.properties";
+		if(args.length==1){propFile=args[0];}
+			
+		ConfigLoader.add(propFile);
+		Configuration config = ConfigLoader.init();
+			
+		String fnTemplate = config.getString("wiki.processor.test.template.kv");
+		Template template = (Template)JaxbUtil.loadJAXB(fnTemplate, Template.class);
+		
+		WikiTemplateKeyValueTransformator kvTransformator = new WikiTemplateKeyValueTransformator();
+		Element e = kvTransformator.transform(template.getMarkup().getValue());
+		JDomUtil.debug(e);
 	}
 }
