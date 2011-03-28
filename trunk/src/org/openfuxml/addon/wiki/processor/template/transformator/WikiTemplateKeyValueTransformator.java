32cc0da36ca52c9d39a1f4169a28ece218549b3a
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-03-28 13:49:07 +0000

    keys are transformed to paragrpah

--- trunk/src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
+++ trunk/src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
@@ -1,7 +1,7 @@
 package org.openfuxml.addon.wiki.processor.template.transformator;
 
-import net.sf.exlp.event.LogEventHandler;
-import net.sf.exlp.event.handler.EhDebug;
+import net.sf.exlp.event.LogEvent;
+import net.sf.exlp.event.handler.EhResultContainer;
 import net.sf.exlp.io.ConfigLoader;
 import net.sf.exlp.io.LoggerInit;
 import net.sf.exlp.listener.LogListener;
@@ -16,6 +16,7 @@ import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
 import org.jdom.Namespace;
 import org.openfuxml.addon.wiki.data.jaxb.Template;
+import org.openfuxml.addon.wiki.processor.template.exlp.event.WikiKeyValueEvent;
 import org.openfuxml.addon.wiki.processor.template.exlp.parser.WikiKeyValueParser;
 import org.openfuxml.addon.wiki.processor.util.AbstractWikiProcessor;
 import org.openfuxml.addon.wiki.processor.util.WikiProcessor;
@@ -33,14 +34,27 @@ public class WikiTemplateKeyValueTransformator extends AbstractWikiProcessor imp
 	
 	public Element transform(String wikiMarkup)
 	{
-		LogEventHandler leh = new EhDebug();
+		EhResultContainer leh = new EhResultContainer();
 		LogParser lp = new WikiKeyValueParser(leh);
 		LogListener ll = new LogListenerString(wikiMarkup,lp);
 		ll.processSingle();
 		
-		Element e = new Element("paragraph");
+		logger.debug(leh.getAlResults().size());
+		
+		Element e = new Element("section");
 		e.setNamespace(nsOfx);
+		e.setAttribute("transparent","true");
 		e.setText("This is the content!!");
+		
+		for(LogEvent logEvent : leh.getAlResults())
+		{
+			WikiKeyValueEvent kvEvent= (WikiKeyValueEvent)logEvent;
+			Element eKv = new Element("paragraph");
+			eKv.setNamespace(nsOfx);
+			eKv.setText(kvEvent.getKv().getKey()+": ");
+			e.addContent(eKv);
+		}
+		
 		return e;
 	}
 	
