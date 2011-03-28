b83447ca69d1a1ff2061975629b4b47465352a0f
thorsten-k <t.kisner@web.de>
2011-03-28 12:52:27 +0000

    WikiKeyValueParser added with new exlp

--- src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
+++ src/org/openfuxml/addon/wiki/processor/template/transformator/WikiTemplateKeyValueTransformator.java
@@ -1,7 +1,12 @@
 package org.openfuxml.addon.wiki.processor.template.transformator;
 
+import net.sf.exlp.event.LogEventHandler;
+import net.sf.exlp.event.handler.EhDebug;
 import net.sf.exlp.io.ConfigLoader;
 import net.sf.exlp.io.LoggerInit;
+import net.sf.exlp.listener.LogListener;
+import net.sf.exlp.listener.impl.LogListenerString;
+import net.sf.exlp.parser.LogParser;
 import net.sf.exlp.util.xml.JDomUtil;
 import net.sf.exlp.util.xml.JaxbUtil;
 
@@ -11,6 +16,7 @@ import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
 import org.jdom.Namespace;
 import org.openfuxml.addon.wiki.data.jaxb.Template;
+import org.openfuxml.addon.wiki.processor.template.exlp.parser.WikiKeyValueParser;
 import org.openfuxml.addon.wiki.processor.util.AbstractWikiProcessor;
 import org.openfuxml.addon.wiki.processor.util.WikiProcessor;
 
@@ -27,7 +33,11 @@ public class WikiTemplateKeyValueTransformator extends AbstractWikiProcessor imp
 	
 	public Element transform(String wikiMarkup)
 	{
-		logger.debug(wikiMarkup);
+		LogEventHandler leh = new EhDebug();
+		LogParser lp = new WikiKeyValueParser(leh);
+		LogListener ll = new LogListenerString(wikiMarkup,lp);
+		ll.processSingle();
+		
 		Element e = new Element("paragraph");
 		e.setNamespace(nsOfx);
 		e.setText("This is the content!!");
