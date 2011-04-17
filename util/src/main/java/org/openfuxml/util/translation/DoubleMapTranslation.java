5b12e5210f32c3c014dee4d4978e50aea5daed22
thorsten-k <t.kisner@web.de>
2011-04-17 09:12:21 +0000

    Exceptions added

---
+++ util/src/main/java/org/openfuxml/util/translation/DoubleMapTranslation.java
@@ -0,0 +1,15 @@
+package org.openfuxml.util.translation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DoubleMapTranslation
+{
+	static Log logger = LogFactory.getLog(DoubleMapTranslation.class);
+	
+	public String translate(String key, String lang)
+	{
+		
+		return "NYI: "+key+" ("+lang+")";
+	}
+}
