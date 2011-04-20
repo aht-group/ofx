5c54bb23d0153a31d0f832b5260fbe0e7ff3ca40
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-04-20 13:54:53 +0000

    Prototype for html template/renderer processing

--- trunk/core/src/main/java/org/openfuxml/renderer/processor/html/OfxHtmlRenderer.java
+++ trunk/core/src/main/java/org/openfuxml/renderer/processor/html/OfxHtmlRenderer.java
@@ -2,6 +2,7 @@ package org.openfuxml.renderer.processor.html;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import net.sf.exlp.util.xml.JDomUtil;
 import net.sf.exlp.util.xml.JaxbUtil;
@@ -9,11 +10,16 @@ import net.sf.exlp.util.xml.JaxbUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
 import org.openfuxml.content.ofx.Ofxdoc;
+import org.openfuxml.content.ofx.Section;
 import org.openfuxml.exception.OfxAuthoringException;
 import org.openfuxml.exception.OfxConfigurationException;
 import org.openfuxml.renderer.util.OfxRenderConfiguration;
 import org.openfuxml.xml.renderer.cmp.Html;
+import org.openfuxml.xml.renderer.html.Renderer;
 import org.openfuxml.xml.renderer.html.Template;
 
 public class OfxHtmlRenderer
@@ -41,16 +47,32 @@ public class OfxHtmlRenderer
 			for(Template template : html.getTemplate())
 			{
 				File fTemplate = cmpConfigUtil.getFile(html.getDirs(), HtmlDir.template.toString(), template.getFileCode());
-				processTemplate(template, fTemplate);
+				for(Object o : ofxdoc.getContent().getContent())
+				{
+					if(o instanceof Section){processTemplate((Section)o, ofxdoc, template, fTemplate);}
+				}
 			}
-			
 		}
 		catch (FileNotFoundException e) {logger.error(e);}
 	}
 	
-	private void processTemplate(Template template, File fTemplate)
+	private void processTemplate(Section section, Ofxdoc ofxdoc, Template template, File fTemplate)
 	{
 		Document doc = JDomUtil.load(fTemplate);
-		JDomUtil.debug(doc);
+		try
+		{
+			XPath xpath = XPath.newInstance( "//ofx:renderer");
+			List<Object> list = xpath.selectNodes(doc);
+			logger.debug(list.size()+" Elements found");
+			for(Object o : list)
+			{
+				Element e = (Element)o;
+				Renderer r = (Renderer)JDomUtil.toJaxb(e, Renderer.class);
+				logger.debug(e.getAttribute("code"));
+				JaxbUtil.debug(r);
+				JaxbUtil.debug(section);
+			}
+		}
+		catch (JDOMException e) {logger.error(e);}
 	}
 }
