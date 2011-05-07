4a8143b06d0daebce175abe6108c014fee63156f
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-05-07 10:46:50 +0000

    Test class for ContainerMerger added

--- trunk/core/src/test/java/org/openfuxml/test/renderer/pre/TestContainerMerger.java
+++ trunk/core/src/test/java/org/openfuxml/test/renderer/pre/TestContainerMerger.java
@@ -1,45 +1,42 @@
-package org.openfuxml.test.renderer.html.section;
+package org.openfuxml.test.renderer.pre;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Collection;
 
 import net.sf.exlp.util.io.LoggerInit;
-import net.sf.exlp.util.xml.JDomUtil;
 import net.sf.exlp.util.xml.JaxbUtil;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.output.Format;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openfuxml.content.ofx.Section;
+import org.openfuxml.content.ofx.Ofxdoc;
 import org.openfuxml.exception.OfxConfigurationException;
 import org.openfuxml.exception.OfxInternalProcessingException;
-import org.openfuxml.renderer.processor.html.section.DefaultSectionRenderer;
+import org.openfuxml.renderer.processor.pre.OfxContainerMerger;
 import org.openfuxml.test.renderer.AbstractFileProcessingTest;
+import org.openfuxml.xml.ns.OfxNsPrefixMapper;
 
 @RunWith(Parameterized.class)
-public class TestDefaultSectionRenderer extends AbstractFileProcessingTest
+public class TestContainerMerger extends AbstractFileProcessingTest
 {
-	static Log logger = LogFactory.getLog(TestDefaultSectionRenderer.class);
+	static Log logger = LogFactory.getLog(TestContainerMerger.class);
 	
-	private DefaultSectionRenderer renderer;
+	private OfxContainerMerger containerMerger;
 	
-	public static String srcDirName = "src/test/resources/data/html/section/ofx";
-	public static final String dstDirName = "src/test/resources/data/html/section/web";
+	public static String srcDirName = "src/test/resources/data/pre/container/in";
+	public static final String dstDirName = "src/test/resources/data/pre/container/out";
 	
-	public TestDefaultSectionRenderer(File fTest)
+	public TestContainerMerger(File fTest)
 	{
 		this.fTest = fTest;
 		String name = fTest.getName().substring(0, fTest.getName().length()-4);
-		fRef = new File(dstDirName,name+".html");
+		fRef = new File(dstDirName,name+".xml");
 	}
 	
 	@Parameterized.Parameters
@@ -48,13 +45,13 @@ public class TestDefaultSectionRenderer extends AbstractFileProcessingTest
 	@Before
 	public void init() throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
 	{	
-		renderer = new DefaultSectionRenderer();
+		containerMerger = new OfxContainerMerger();
 	}
 	
 	@After
 	public void close()
 	{
-		renderer = null;
+		containerMerger = null;
 	}
     
     @Test
@@ -63,25 +60,21 @@ public class TestDefaultSectionRenderer extends AbstractFileProcessingTest
     	render(false);
     }
 	
-	private void render(boolean saveReference) throws FileNotFoundException
+	private void render(boolean saveReference) throws FileNotFoundException, OfxInternalProcessingException
 	{
 		logger.debug(fTest.getAbsoluteFile());
-		Section section = (Section)JaxbUtil.loadJAXB(fTest.getAbsolutePath(), Section.class);
+		Ofxdoc ofxSrc = (Ofxdoc)JaxbUtil.loadJAXB(fTest.getAbsolutePath(), Ofxdoc.class);
 
-		Element html = new Element("html");
-		html.addContent(renderer.render(section));
-		
-		Document doc = new Document();
-		doc.setRootElement(html);
+		Ofxdoc ofxDst = containerMerger.merge(ofxSrc);
 		
 		if(saveReference)
 		{
-			JDomUtil.save(doc, fRef, Format.getPrettyFormat());
+			JaxbUtil.save(fRef, ofxDst, new OfxNsPrefixMapper(), true);
 		}
 		else
 		{
-			Document docRef = JDomUtil.load(fRef);
-			Assert.assertEquals(JDomUtil.toString(docRef),JDomUtil.toString(doc));
+			Ofxdoc ofxRef = (Ofxdoc)JaxbUtil.loadJAXB(fRef.getAbsolutePath(), Ofxdoc.class);
+			Assert.assertEquals(JaxbUtil.toString(ofxRef),JaxbUtil.toString(ofxDst));
 		}	
 	}
 	
@@ -95,11 +88,11 @@ public class TestDefaultSectionRenderer extends AbstractFileProcessingTest
 		int id = -1;
 		int index = 0;
 		
-		for(Object[] o : TestDefaultSectionRenderer.initFileNames())
+		for(Object[] o : TestContainerMerger.initFileNames())
 		{
 			File fTest = (File)o[0];
 		
-			TestDefaultSectionRenderer test = new TestDefaultSectionRenderer(fTest);
+			TestContainerMerger test = new TestContainerMerger(fTest);
 			test.init();
 			logger.trace(id+" "+index);
 			if(id<0 | id==index){test.render(saveReference);}
