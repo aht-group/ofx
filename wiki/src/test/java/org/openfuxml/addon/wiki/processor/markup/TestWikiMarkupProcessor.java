9ca959da5f5228def58211161050c872c7016c2c
thorsten-k <t.kisner@web.de>
2011-12-02 08:05:53 +0000

    Fixed TestWikiMarkupProcessor (OFX-7)

--- wiki/src/test/java/org/openfuxml/addon/wiki/processor/markup/TestWikiMarkupProcessor.java
+++ wiki/src/test/java/org/openfuxml/addon/wiki/processor/markup/TestWikiMarkupProcessor.java
@@ -1,11 +1,10 @@
-package org.openfuxml.test.addon.wiki;
+package org.openfuxml.addon.wiki.processor.markup;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.io.StringIO;
 import net.sf.exlp.util.xml.JaxbUtil;
 
@@ -14,21 +13,21 @@ import org.apache.commons.logging.LogFactory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openfuxml.addon.wiki.data.jaxb.MarkupProcessor;
 import org.openfuxml.addon.wiki.data.jaxb.Templates;
-import org.openfuxml.addon.wiki.processor.markup.WikiMarkupProcessor;
 import org.openfuxml.exception.OfxConfigurationException;
 import org.openfuxml.exception.OfxInternalProcessingException;
+import org.openfuxml.test.AbstractOfxWikiTest;
+import org.openfuxml.test.OfxWikiTstBootstrap;
 import org.openfuxml.xml.renderer.cmp.Cmp;
 
 @RunWith(Parameterized.class)
-public class TestMarkupProcessor
+public class TestWikiMarkupProcessor extends AbstractOfxWikiTest
 {
-	static Log logger = LogFactory.getLog(TestMarkupProcessor.class);
+	static Log logger = LogFactory.getLog(TestWikiMarkupProcessor.class);
 	
 	private WikiMarkupProcessor wmp;
 	
@@ -38,7 +37,7 @@ public class TestMarkupProcessor
 	private File fTest;
 	private File fRef;
 
-	public TestMarkupProcessor(File fTest)
+	public TestWikiMarkupProcessor(File fTest)
 	{
 		this.fTest = fTest;
 		String name = fTest.getName().substring(0, fTest.getName().length()-4);
@@ -60,14 +59,6 @@ public class TestMarkupProcessor
 		}
 		return c;
 	}
-	
-	@BeforeClass
-    public static void initLogger()
-	{
-		LoggerInit loggerInit = new LoggerInit("log4junit.xml");	
-		loggerInit.addAltPath("src/test/resources/config");
-		loggerInit.init();
-    }
 	
 	@Before
 	public void initWmp() throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
@@ -107,26 +98,19 @@ public class TestMarkupProcessor
 		}	
 	}
 	
-	public static void chain() throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
-	{
-		for(Object[] o : TestMarkupProcessor.initFileNames())
+	public static void main(String[] args) throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
+    {
+		OfxWikiTstBootstrap.init();	
+		
+		for(Object[] o : TestWikiMarkupProcessor.initFileNames())
 		{
 			File fTest = (File)o[0];
 			logger.debug(fTest);
 			
-			TestMarkupProcessor test = new TestMarkupProcessor(fTest);
+			TestWikiMarkupProcessor test = new TestWikiMarkupProcessor(fTest);
 			test.initWmp();
 			test.wikiPlainToMarkup(true);
 			test.closeWmp();
 		}
-	}
-	
-	public static void main(String[] args) throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
-    {
-		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
-			loggerInit.addAltPath("src/test/resources/config");
-			loggerInit.init();	
-		
-		TestMarkupProcessor.chain();
     }
 }
