7b869a2df800969ac0e70610074ed6df2c353942
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-12-09 12:33:12 +0000

    TestLatexTabularUtil added (OFX-24)

---
+++ trunk/core/src/test/java/org/openfuxml/renderer/processor/latex/content/table/util/TestLatexTabularUtil.java
@@ -0,0 +1,83 @@
+package org.openfuxml.renderer.processor.latex.content.table.util;
+
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openfuxml.content.ofx.layout.Width;
+import org.openfuxml.content.ofx.table.Column;
+import org.openfuxml.content.ofx.table.Columns;
+import org.openfuxml.exception.OfxAuthoringException;
+import org.openfuxml.test.AbstractOfxCoreTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLatexTabularUtil extends AbstractOfxCoreTest
+{	
+	final static Logger logger = LoggerFactory.getLogger(TestLatexTabularUtil.class);
+	
+	private Columns cols;
+	
+	@Before
+	public void initRenderer()
+	{
+		cols = createColumns();
+	}
+		
+	public static Columns createColumns()
+	{
+		Columns cols = new Columns();
+		
+		cols.getColumn().add(createCol(10));
+		cols.getColumn().add(createCol(20));
+		
+		return cols;
+	}
+	
+	public static Column createCol(int relative)
+	{
+		Width width = new Width();
+		width.setValue(relative);
+		Column col = new Column();
+		col.setWidth(width);
+		
+		return col;
+	}
+	
+    @Test
+    public void getColDefinition() throws IOException, OfxAuthoringException
+    {    	    	
+    	LatexTabluarUtil tabUtil = new LatexTabluarUtil(createColumns());
+    	Assert.assertEquals("p{\\tabLenA}", tabUtil.getColDefinition(0));
+    	Assert.assertEquals("p{\\tabLenB}", tabUtil.getColDefinition(1));
+    }
+    
+    @Test
+    public void divide() throws IOException, OfxAuthoringException
+    {    	    	
+    	LatexTabluarUtil tabUtil = new LatexTabluarUtil(createColumns());
+    	for(int i=0;i<cols.getColumn().size();i++)
+    	{
+    		Assert.assertEquals((int)cols.getColumn().get(i).getWidth().getValue()*LatexTabluarUtil.muliplier, tabUtil.getMultiplier(i));
+    	}
+    }
+    
+    @Test
+    public void multiplier() throws IOException, OfxAuthoringException
+    {    	    	
+    	int expected = 30*LatexTabluarUtil.muliplier;
+    	LatexTabluarUtil tabUtil = new LatexTabluarUtil(createColumns());
+    	Assert.assertEquals(expected, tabUtil.getDivide());
+    } 
+    
+    @Test
+    public void lenghtCla() throws IOException, OfxAuthoringException
+    {    	    	
+    	LatexTabluarUtil tabUtil = new LatexTabluarUtil(createColumns());
+    	List<String> list = tabUtil.getLatexLengthCalculations();
+    	for(String s : list){logger.debug(s);}
+    }
+}
