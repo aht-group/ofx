5d6466b8d3be6424f6ab236574b2e178c9079dff
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2011-12-25 16:01:52 +0000

    XSD for graph processing added (OFX-28)

---
+++ trunk/xml/src/test/java/org/openfuxml/xml/addon/graph/AbstractXmlGraphTest.java
@@ -0,0 +1,11 @@
+package org.openfuxml.xml.addon.graph;
+
+import java.io.File;
+
+import org.openfuxml.test.AbstractOfxXmlTest;
+
+public abstract class AbstractXmlGraphTest extends AbstractOfxXmlTest
+{
+	protected static final String rootDir = "src/test/resources/data/xml/addon/graph";
+	protected static File fXml;
+}
