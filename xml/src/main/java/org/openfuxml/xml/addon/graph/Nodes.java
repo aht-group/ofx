45e0c3b2c8e2e510caf63d030e7f7a1466849bf6
thorsten-k <t.kisner@web.de>
2012-01-20 17:15:18 +0000

    Added XML for graph definitions (OFX-30)

--- xml/src/main/java/org/openfuxml/xml/addon/graph/Nodes.java
+++ xml/src/main/java/org/openfuxml/xml/addon/graph/Nodes.java
@@ -9,6 +9,8 @@
 package org.openfuxml.xml.addon.graph;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -26,7 +28,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{http://www.openfuxml.org/graph}node"/>
+ *         &lt;element ref="{http://www.openfuxml.org/graph}node" maxOccurs="unbounded"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -39,41 +41,50 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "", propOrder = {
     "node"
 })
-@XmlRootElement(name = "tree")
-public class Tree
+@XmlRootElement(name = "nodes")
+public class Nodes
     implements Serializable
 {
 
     private final static long serialVersionUID = 1L;
     @XmlElement(required = true)
-    protected Node node;
+    protected List<Node> node;
 
     /**
      * Gets the value of the node property.
      * 
-     * @return
-     *     possible object is
-     *     {@link Node }
-     *     
-     */
-    public Node getNode() {
-        return node;
-    }
-
-    /**
-     * Sets the value of the node property.
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the node property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link Node }
-     *     
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getNode().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Node }
+     * 
+     * 
      */
-    public void setNode(Node value) {
-        this.node = value;
+    public List<Node> getNode() {
+        if (node == null) {
+            node = new ArrayList<Node>();
+        }
+        return this.node;
     }
 
     public boolean isSetNode() {
-        return (this.node!= null);
+        return ((this.node!= null)&&(!this.node.isEmpty()));
+    }
+
+    public void unsetNode() {
+        this.node = null;
     }
 
 }
---
+++ xml/src/test/java/org/openfuxml/xml/addon/graph/TestXmlEdge.java
@@ -0,0 +1,51 @@
+package org.openfuxml.xml.addon.graph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openfuxml.test.OfxXmlTstBootstrap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestXmlEdge extends AbstractXmlGraphTest
+{	
+	final static Logger logger = LoggerFactory.getLogger(TestXmlEdge.class);
+	
+	@BeforeClass
+	public static void initFiles()
+	{
+		fXml = new File(rootDir,"edge.xml");
+	}
+    
+    @Test
+    public void testXml() throws FileNotFoundException
+    {
+    	Edge actual = create(true);
+    	Edge expected = JaxbUtil.loadJAXB(fXml.getAbsolutePath(), Edge.class);
+    	assertJaxbEquals(expected, actual);
+    }
+   
+    public static Edge create(boolean withChilds)
+    {
+    	Edge xml = new Edge();
+    	xml.setId(123);
+    	xml.setFrom(1);
+    	xml.setTo(2);	
+    	return xml;
+    }
+    
+    public void save() {save(create(true),fXml,false);}
+	
+	public static void main(String[] args)
+    {
+		OfxXmlTstBootstrap.init();
+			
+		TestXmlEdge.initFiles();	
+		TestXmlEdge test = new TestXmlEdge();
+		test.save();
+    }
+}
