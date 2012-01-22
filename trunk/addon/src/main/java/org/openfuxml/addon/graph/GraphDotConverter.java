1db19e9c518b0f02bb7ab0fe1270250266a5e936
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2012-01-22 08:15:53 +0000

    Graph to Dot converter added (OFX-31)

---
+++ trunk/addon/src/main/java/org/openfuxml/addon/graph/GraphDotConverter.java
@@ -0,0 +1,57 @@
+package org.openfuxml.addon.graph;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.openfuxml.xml.addon.graph.Edge;
+import org.openfuxml.xml.addon.graph.Graph;
+import org.openfuxml.xml.addon.graph.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GraphDotConverter
+{
+	final static Logger logger = LoggerFactory.getLogger(GraphDotConverter.class);
+	
+	private final static String ls = "\n";
+	private final static String q = "\"";
+	
+	private StringBuffer sb;
+	private String code,label;
+	
+	private Map<Long,Node> mapNodes;
+	
+	public GraphDotConverter(String code, String label)
+	{
+		this.code=code;
+		this.label=label;
+		
+		mapNodes = new Hashtable<Long,Node>();
+		sb = new StringBuffer();
+	}
+	
+	public String convert(Graph ofxGraph)
+	{
+		for(Node n : ofxGraph.getNodes().getNode()){mapNodes.put(n.getId(), n);}
+		
+		sb.append("digraph ").append(label).append(" {").append(ls);
+		
+		for(Edge e : ofxGraph.getEdges().getEdge())
+		{
+			Node nSrc = mapNodes.get(e.getFrom());
+			Node nDst = mapNodes.get(e.getTo());
+			
+			sb.append(q).append(nSrc.getLabel()).append(q);
+			sb.append(" -> ");
+			sb.append(q).append(nDst.getLabel()).append(q);
+			sb.append(" ");
+			
+			if(!e.isDirected()){sb.append("[dir=none]");}
+		
+			sb.append(";").append(ls);
+		}
+		
+		sb.append("}").append(ls);
+		return sb.toString();	
+	}
+}
