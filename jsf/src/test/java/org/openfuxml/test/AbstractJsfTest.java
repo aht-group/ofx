492170748fc18607e3d34ba1e1e9c60dc2ba570f
thorsten-k <t.kisner@web.de>
2013-03-25 14:10:10 +0000

    dependency to ahtutils removed

--- jsf/src/test/java/org/openfuxml/test/AbstractJsfTest.java
+++ jsf/src/test/java/org/openfuxml/test/AbstractJsfTest.java
@@ -1,15 +1,20 @@
 package org.openfuxml.test;
 
-import net.sf.ahtutils.test.AbstractAhtUtilsXmlTest;
+import java.io.File;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import net.sf.exlp.util.DateUtil;
 import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.openfuxml.xml.OfxNsPrefixMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AbstractJsfTest extends AbstractAhtUtilsXmlTest
+public class AbstractJsfTest
 {
 	final static Logger logger = LoggerFactory.getLogger(AbstractJsfTest.class);	
 	
@@ -21,4 +26,23 @@ public class AbstractJsfTest extends AbstractAhtUtilsXmlTest
 			loggerInit.init();
 		JaxbUtil.setNsPrefixMapper(new OfxNsPrefixMapper());
     }
+	
+	//****Copy from AbstractAhtUtilsXmlTest ******
+	protected void assertJaxbEquals(Object expected, Object actual)
+	{
+		Assert.assertEquals("Actual XML differes from expected XML",JaxbUtil.toString(expected),JaxbUtil.toString(actual));
+	}
+	
+	protected static XMLGregorianCalendar getDefaultXmlDate()
+	{
+		return DateUtil.getXmlGc4D(DateUtil.getDateFromInt(2011, 11, 11, 11, 11, 11));
+	}
+	
+	protected void save(Object xml, File f)
+	{
+		logger.debug("Saving Reference XML");
+		JaxbUtil.debug(xml);
+    	JaxbUtil.save(f, xml, true);
+	}
+	//***********************************
 }
