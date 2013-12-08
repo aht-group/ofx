3c71d90378e402291a1a81f8c7305244eabd6f73
thorsten-k <t.kisner@web.de>
2013-12-08 14:38:05 +0000

    SectionXpath to get section with id (OFX-41)

--- xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
+++ xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
@@ -1,16 +1,19 @@
 package org.openfuxml.xml.xpath.content;
 
-import net.sf.exlp.exception.ExlpXpathNotFoundException;
+import java.util.List;
 
+import net.sf.exlp.exception.ExlpXpathNotFoundException;
+import net.sf.exlp.exception.ExlpXpathNotUniqueException;
+
+import org.apache.commons.jxpath.JXPathContext;
 import org.openfuxml.content.ofx.Section;
 import org.openfuxml.content.ofx.Title;
-import org.openfuxml.xml.xpath.cmp.HtmlXpath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SectionXpath
 {
-	final static Logger logger = LoggerFactory.getLogger(HtmlXpath.class);
+	final static Logger logger = LoggerFactory.getLogger(SectionXpath.class);
 	
 	public static synchronized Title getTitle(Section section) throws ExlpXpathNotFoundException
 	{
@@ -23,4 +26,14 @@ public class SectionXpath
 		}
 		throw new ExlpXpathNotFoundException("No "+Title.class.getSimpleName()+" in this section");
 	}
+
+    public static synchronized Section getRenderer(Section section, String id) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+    {
+        JXPathContext context = JXPathContext.newContext(section);
+        List<Section> listResult = (List<Section>)context.selectNodes("//section[@id='"+id+"']");
+        if(listResult.size()==0){throw new ExlpXpathNotFoundException("No "+Section.class.getSimpleName()+" found for id="+id);}
+        else if(listResult.size()>1){throw new ExlpXpathNotUniqueException("No unique "+Section.class.getSimpleName()+" for id="+id);}
+
+        return listResult.get(0);
+    }
 }
