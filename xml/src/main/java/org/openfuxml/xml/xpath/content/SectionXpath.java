e974e31d0818c85845251cc3fcdf2bf2c1f26908
thorsten-k <t.kisner@web.de>
2014-07-17 06:50:25 +0000

    SectionXpath (for listings) added

--- xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
+++ xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
@@ -1,8 +1,12 @@
 package org.openfuxml.xml.xpath.content;
 
+import java.util.List;
+
 import net.sf.exlp.exception.ExlpXpathNotFoundException;
 import net.sf.exlp.exception.ExlpXpathNotUniqueException;
 
+import org.apache.commons.jxpath.JXPathContext;
+import org.openfuxml.content.ofx.Listing;
 import org.openfuxml.content.ofx.Section;
 import org.openfuxml.content.ofx.Title;
 import org.slf4j.Logger;
@@ -42,4 +46,18 @@ public class SectionXpath
     	if(result==null){throw new ExlpXpathNotFoundException("No "+Section.class.getSimpleName()+" found for id="+id);}
     	return result;
     }
+    
+    public static synchronized Listing getSeed(Section section, String id) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	{
+		JXPathContext context = JXPathContext.newContext(section);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("//*[@id='"+id+"']");
+		
+		@SuppressWarnings("unchecked")
+		List<Listing> listResult = (List<Listing>)context.selectNodes(sb.toString());
+		if(listResult.size()==0){throw new ExlpXpathNotFoundException("No "+Listing.class.getSimpleName()+" for id="+id);}
+		else if(listResult.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Listing.class.getSimpleName()+" for id="+id);}
+		return listResult.get(0);
+	}
 }
