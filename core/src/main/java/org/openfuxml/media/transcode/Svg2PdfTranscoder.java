678cdcfd57a62d05202c3907e1297914855f1dbe
thorsten-k <t.kisner@web.de>
2014-07-24 06:19:32 +0000

    Transcoder now with static processing svg -> pdf

--- core/src/main/java/org/openfuxml/media/transcode/Svg2PdfTranscoder.java
+++ core/src/main/java/org/openfuxml/media/transcode/Svg2PdfTranscoder.java
@@ -1,9 +1,11 @@
 package org.openfuxml.media.transcode;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import net.sf.exlp.util.io.resourceloader.MultiResourceLoader;
@@ -41,32 +43,34 @@ public class Svg2PdfTranscoder implements CrossMediaTranscoder
 		CrossMediaFileUtil.createParentDirs(file);
 		logger.info("Transcoding to "+file.getAbsolutePath());
 		
-		
-	       
-			try
-			{
-				 TranscoderInput input_svg_image = new TranscoderInput(mrl.searchIs(media.getSrc()));        
-			        //Step-2: Define OutputStream to PDF file and attach to TranscoderOutput
-				 OutputStream pdf_ostream = new FileOutputStream(file);
-				 TranscoderOutput output_pdf_file = new TranscoderOutput(pdf_ostream);               
-			        // Step-3: Create a PDF Transcoder and define hints
-			        Transcoder transcoder = new PDFTranscoder();
-			        // Step-4: Write output to PDF format
-			        transcoder.transcode(input_svg_image, output_pdf_file);
-			        // Step 5- close / flush Output Stream
-			        pdf_ostream.flush();
-			        pdf_ostream.close();      
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (TranscoderException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	          
+		try
+		{
+			InputStream is = mrl.searchIs(media.getSrc());
+			Svg2PdfTranscoder.transcode(is,file);
+		}
+		catch (FileNotFoundException e){e.printStackTrace();}
+		catch (IOException e) {e.printStackTrace();}
+		catch (TranscoderException e) {e.printStackTrace();}
+	}
+	
+	public static void transcode(File fSvg, File fPdf) throws TranscoderException, IOException
+	{
+		InputStream is = new FileInputStream(fSvg);
+		Svg2PdfTranscoder.transcode(is, fPdf);
+	}
+	
+	public static void transcode(InputStream is, File fPdf) throws TranscoderException, IOException
+	{
+		TranscoderInput tiSvg = new TranscoderInput(is);        
+
+		OutputStream os = new FileOutputStream(fPdf);
+		TranscoderOutput toPdf = new TranscoderOutput(os);               
+
+		Transcoder transcoder = new PDFTranscoder();
+		transcoder.transcode(tiSvg, toPdf);
+
+		os.flush();
+		os.close(); 
 	}
 	
 	@Override
