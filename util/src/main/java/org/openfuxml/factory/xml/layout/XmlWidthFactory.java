19e69047fe8dfda57b5b2c0f2ccb6422ab744168
thorsten-k <t.kisner@web.de>
2015-01-06 11:58:58 +0000

    XmlWidthFactory extended with percentage

--- util/src/main/java/org/openfuxml/factory/xml/layout/XmlWidthFactory.java
+++ util/src/main/java/org/openfuxml/factory/xml/layout/XmlWidthFactory.java
@@ -4,13 +4,21 @@ import org.openfuxml.content.layout.Width;
 
 public class XmlWidthFactory
 {
-	public static enum Unit {cm}
+	public static enum Unit {cm,percentage}
 	
 	public static Width cm(double value)
 	{
 		Width xml = new Width();
 		xml.setValue(value);
 		xml.setUnit(Unit.cm.toString());
+		return xml;
+	}
+	
+	public static Width percentage(double value)
+	{
+		Width xml = new Width();
+		xml.setValue(value);
+		xml.setUnit(Unit.percentage.toString());
 		return xml;
 	}
 }
