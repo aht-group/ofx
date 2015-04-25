a0df9110809ac816cf817c48c268bfc070b1c356
thorsten-k <t.kisner@web.de>
2015-04-25 10:57:44 +0000

    transcoder for app engine crontab added

---
+++ util/src/main/java/org/openfuxml/trancoder/xml/AppEngineTranscoder.java
@@ -0,0 +1,115 @@
+package org.openfuxml.trancoder.xml;
+
+import java.io.FileNotFoundException;
+
+import net.sf.exlp.util.io.resourceloader.MultiResourceLoader;
+import net.sf.exlp.util.xml.JDomUtil;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.openfuxml.content.ofx.Comment;
+import org.openfuxml.content.table.Body;
+import org.openfuxml.content.table.Columns;
+import org.openfuxml.content.table.Content;
+import org.openfuxml.content.table.Head;
+import org.openfuxml.content.table.Row;
+import org.openfuxml.content.table.Specification;
+import org.openfuxml.content.table.Table;
+import org.openfuxml.factory.xml.ofx.content.XmlCommentFactory;
+import org.openfuxml.factory.xml.ofx.content.text.XmlTitleFactory;
+import org.openfuxml.factory.xml.table.OfxCellFactory;
+import org.openfuxml.factory.xml.table.OfxColumnFactory;
+import org.openfuxml.util.OfxCommentBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppEngineTranscoder
+{
+	final static Logger logger = LoggerFactory.getLogger(AppEngineTranscoder.class);
+	
+	private MultiResourceLoader mrl;
+	
+	public AppEngineTranscoder()
+	{
+		mrl = new MultiResourceLoader();
+	}
+	
+	public Table cron(String fileName) throws FileNotFoundException
+	{
+		Document doc = JDomUtil.load(mrl.searchIs(fileName));
+
+		Table table = new Table();
+		table.setSpecification(createSpecifications());
+		table.setContent(createContent(doc));
+		table.getContent().setHead(buildHead());
+		
+		table.setId("my.id");
+		table.setComment(comment(table.getId()));
+		
+		table.setTitle(XmlTitleFactory.build("Cron Jobs"));
+		
+		return table;
+	}
+	
+	private Content createContent(Document doc)
+	{
+		Body body = new Body();
+
+		Element rootNode = doc.getRootElement();
+		
+		for(Element cron : rootNode.getChildren("cron"))
+		{
+			body.getRow().add(createRow(cron));
+		}
+		
+		Content content = new Content();
+		content.getBody().add(body);
+		
+		return content;
+	}
+	
+	private Row createRow(Element cron) 
+	{		
+		Row row = new Row();
+		
+		row.getCell().add(OfxCellFactory.createParagraphCell(cron.getChildText("url")));
+		row.getCell().add(OfxCellFactory.createParagraphCell(cron.getChildText("schedule")));
+		row.getCell().add(OfxCellFactory.createParagraphCell(cron.getChildText("description")));
+		return row;
+	}
+	
+	private Head buildHead()
+	{
+		Row row = new Row();
+		
+		row.getCell().add(OfxCellFactory.createParagraphCell("URL"));
+		row.getCell().add(OfxCellFactory.createParagraphCell("Schedule"));
+		row.getCell().add(OfxCellFactory.createParagraphCell("Frequency"));
+		
+		Head head = new Head();
+		head.getRow().add(row);
+		return head;
+	}
+	
+	private Specification createSpecifications()
+	{
+		int flexWidth=0;int w=0;
+		Columns cols = new Columns();
+		
+		w=20;flexWidth=flexWidth+w;cols.getColumn().add(OfxColumnFactory.flex(w,true));
+		w=20;flexWidth=flexWidth+w;cols.getColumn().add(OfxColumnFactory.flex(w,true));
+		cols.getColumn().add(OfxColumnFactory.flex());
+		
+		Specification specification = new Specification();
+		specification.setColumns(cols);
+		return specification;
+	}
+	
+	private Comment comment(String id)
+	{
+		Comment comment = XmlCommentFactory.build();
+		OfxCommentBuilder.fixedId(comment, id);
+		OfxCommentBuilder.doNotModify(comment);
+		return comment;
+	}
+}
---
+++ util/src/main/java/org/openfuxml/util/OfxCommentBuilder.java
@@ -0,0 +1,22 @@
+package org.openfuxml.util;
+
+import org.openfuxml.content.ofx.Comment;
+import org.openfuxml.factory.xml.ofx.content.XmlRawFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OfxCommentBuilder
+{
+	final static Logger logger = LoggerFactory.getLogger(OfxCommentBuilder.class);
+	
+	public static void fixedId(Comment comment, String id)
+	{
+		comment.getRaw().add(XmlRawFactory.build("The ID (label) for this element is fixed: "+id));
+	}
+	
+	public static void doNotModify(Comment comment)
+	{
+		comment.getRaw().add(XmlRawFactory.build(""));
+		comment.getRaw().add(XmlRawFactory.build("Do not modify this auto-generated file, it will be overwritten without warning!"));
+	}
+}
