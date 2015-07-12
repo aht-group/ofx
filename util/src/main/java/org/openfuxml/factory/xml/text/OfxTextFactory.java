00425b0d4240d27a0658d21c65486807e383ec2d
thorsten-k <t.kisner@web.de>
2015-07-12 17:59:03 +0000

    OfxTextFactory extended with lang

--- util/src/main/java/org/openfuxml/factory/xml/text/OfxTextFactory.java
+++ util/src/main/java/org/openfuxml/factory/xml/text/OfxTextFactory.java
@@ -4,10 +4,12 @@ import org.openfuxml.content.text.Text;
 
 public class OfxTextFactory
 {
-	public static Text build(String text)
+	public static Text build(String text){return build(null,text);}
+	public static Text build(String lang, String text)
 	{
 		Text xml = new Text();
 		xml.setValue(text);
+		xml.setLang(lang);
 		return xml;
 	}
 }
