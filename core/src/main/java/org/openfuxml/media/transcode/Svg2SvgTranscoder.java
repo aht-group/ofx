c660282a59157a4969dd5173a80af3a5e8265b5a
thorsten-k <t.kisner@web.de>
2015-07-15 08:15:45 +0000

    Svg2SvgTranscoder added

---
+++ core/src/main/java/org/openfuxml/media/transcode/Svg2SvgTranscoder.java
@@ -0,0 +1,26 @@
+package org.openfuxml.media.transcode;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.transcoder.TranscoderException;
+import org.openfuxml.media.cross.LatexCrossMediaManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Svg2SvgTranscoder
+{
+	final static Logger logger = LoggerFactory.getLogger(LatexCrossMediaManager.class);
+	
+	public static byte[] transcode(SVGGraphics2D g) throws IOException, TranscoderException
+	{
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		boolean useCSS = true; // we want to use CSS style attributes
+	    Writer out = new OutputStreamWriter(os, "UTF-8");
+	    g.stream(out, useCSS);
+	    return os.toByteArray();
+	}
+}
