0ad1df8b00e86f6787657777baade83ede7a77f8
thorsten-k <thorsten-k@2788577f-44b2-48b2-8152-f8f1a8eee874>
2015-07-28 04:27:09 +0000

    TableFactory for ResultSets

--- trunk/util/src/main/java/org/openfuxml/factory/xml/table/OfxTableFactory.java
+++ trunk/util/src/main/java/org/openfuxml/factory/xml/table/OfxTableFactory.java
@@ -1,5 +1,12 @@
 package org.openfuxml.factory.xml.table;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.openfuxml.content.table.Body;
 import org.openfuxml.content.table.Content;
 import org.openfuxml.content.table.Head;
@@ -12,21 +19,51 @@ public class OfxTableFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(OfxTableFactory.class);
 	
-	public static Table build(String[] columns, Object[][] data)
+	public static Table build(ResultSet rs) throws SQLException
+	{
+		ResultSetMetaData meta = rs.getMetaData();
+		List<String> header = new ArrayList<String>();
+		for(int i=1;i<=meta.getColumnCount();i++)
+		{
+			header.add(meta.getColumnLabel(i));
+		}
+
+		List<Object[]> data = new ArrayList<Object[]>();
+		while(rs.next())
+		{	
+			Object[] row = new Object[meta.getColumnCount()];
+			for(int i=1;i<meta.getColumnCount();i++)
+			{
+				row[i-1] = rs.getObject(i);
+			}
+			data.add(row);
+		}
+		
+		return build(header,data);
+	}
+	
+	public static Table build(String[] header, Object[][] data)
+	{
+		List<String> listHeader = new ArrayList<String>(Arrays.asList(header));
+		List<Object[]> listData = new ArrayList<Object[]>(Arrays.asList(data));
+		return build(listHeader,listData);
+	}
+	
+	public static Table build(List<String> header, List<Object[]> data)
 	{
 		Table table = new Table();
 		
 		Content content = new Content();
-		content.setHead(buildHead(columns));
+		content.setHead(buildHead(header));
 		content.getBody().add(buildBody(data));
 		table.setContent(content);
 		return table;
 	}
 	
-	private static Head buildHead(String[] columns)
+	private static Head buildHead(List<String> header)
 	{
 		Row row = new Row();
-		for(String column : columns)
+		for(String column : header)
 		{
 			row.getCell().add(OfxCellFactory.createParagraphCell(column));
 		}
@@ -36,7 +73,7 @@ public class OfxTableFactory
 		return head;
 	}
 	
-	private static Body buildBody(Object[][] data)
+	private static Body buildBody(List<Object[]> data)
 	{
 		Body body = new Body();
 		for(Object[] row : data)
@@ -52,7 +89,14 @@ public class OfxTableFactory
 		
 		for(Object o : data)
 		{
-			row.getCell().add(OfxCellFactory.createParagraphCell(o.toString()));
+			if(o!=null)
+			{
+				row.getCell().add(OfxCellFactory.createParagraphCell(o.toString()));
+			}
+			else
+			{
+				row.getCell().add(OfxCellFactory.createParagraphCell(""));
+			}
 		}
 		
 		return row;
