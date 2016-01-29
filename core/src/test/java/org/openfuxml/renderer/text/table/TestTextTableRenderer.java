30005572712c0bb72b9c2e138ebf4aecbf2de0a8
Thorsten <t.kisner@web.de>
2016-01-29 19:54:36 +0100

    TableRenderer with output

--- core/src/test/java/org/openfuxml/renderer/text/table/TestTextTableRenderer.java
+++ core/src/test/java/org/openfuxml/renderer/text/table/TestTextTableRenderer.java
@@ -1,39 +1,42 @@
 package org.openfuxml.renderer.text.table;
 
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.openfuxml.content.table.Table;
 import org.openfuxml.exception.OfxAuthoringException;
 import org.openfuxml.factory.xml.table.OfxTableFactory;
 import org.openfuxml.factory.xml.table.TestTableFactory;
-import org.openfuxml.test.AbstractOfxCoreTest;
+import org.openfuxml.renderer.text.AbstractTestTextRenderer;
 import org.openfuxml.test.OfxCoreTestBootstrap;
 
-public class TestTextTableRenderer extends AbstractOfxCoreTest
+public class TestTextTableRenderer extends AbstractTestTextRenderer
 {	
-	private Table table;
+	private static enum Key {simple}
+	
 	private TextTableRenderer renderer;
 	
 	@Before
 	public void init()
 	{
-		table = OfxTableFactory.build(TestTableFactory.columnNames,TestTableFactory.data);
+		super.initDir("table");
 		renderer = new TextTableRenderer(cmm,dsm);
 	}
 	
 	@Test
-	public void testOfx() throws OfxAuthoringException
+	public void simple() throws OfxAuthoringException, IOException
 	{
-		renderer.render(table);
-		debugCharacter(renderer);
+		initFile(Key.simple);
+		renderer.render(OfxTableFactory.build(TestTableFactory.columnNames,TestTableFactory.data));
+		renderTest(renderer);
 	}
 	
 	public static void main(String[] args) throws Exception
     {
-    	OfxCoreTestBootstrap.init();
-			
-    	TestTextTableRenderer cli = new TestTextTableRenderer();
-    	cli.init();
-    	cli.testOfx();
+    	OfxCoreTestBootstrap.init();	
+    	TestTextTableRenderer test = new TestTextTableRenderer();
+    	test.setEnvironment(true);
+    	
+    	test.init();test.simple();
     }
 }
