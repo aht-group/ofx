835eed2f563568d9149a0cd0476b2b779616fae7
RebeccaRoblick <rebecca.roblick@gmail.com>
2016-02-17 17:00:30 +0100

    rendering marginalia

--- core/src/main/java/org/openfuxml/renderer/html/structure/HtmlMarginaliaRenderer.java
+++ core/src/main/java/org/openfuxml/renderer/html/structure/HtmlMarginaliaRenderer.java
@@ -1,15 +1,11 @@
 package org.openfuxml.renderer.html.structure;
 
-import org.openfuxml.content.media.Image;
 import org.openfuxml.content.ofx.Marginalia;
-import org.openfuxml.content.ofx.Paragraph;
-import org.openfuxml.exception.OfxAuthoringException;
 import org.openfuxml.interfaces.DefaultSettingsManager;
 import org.openfuxml.interfaces.media.CrossMediaManager;
 import org.openfuxml.interfaces.renderer.latex.OfxLatexRenderer;
 import org.openfuxml.renderer.html.AbstractOfxHtmlRenderer;
 import org.openfuxml.renderer.html.HtmlElement;
-import org.openfuxml.renderer.html.media.HtmlImageRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,21 +22,11 @@ public class HtmlMarginaliaRenderer extends AbstractOfxHtmlRenderer implements O
 	{
 		HtmlParagraphRenderer marg = new HtmlParagraphRenderer(cmm,dsm);
 		marg.renderAsMarginalia(parent, marginalia);
-//		for(Object o : marginalia.getContent())
-//		{
-//			if(o instanceof String){marg.addContent(((String)o));}
-//			else if(o instanceof Paragraph){paragraphRenderer(marg, ((Paragraph)o));}
-//			else if(o instanceof Image){imageRenderer(marg, ((Image)o));}
-////			else if(o instanceof Container){}
-//// 			else if(o instanceof Box){}
-//// 			else if(o instanceof Column){}
-//		}
-//		parent.addContent(marg);
 	}
 
-	public void imageRenderer(HtmlElement parent, Image image)
-	{
-		HtmlImageRenderer iRenderer = new HtmlImageRenderer(cmm, dsm);
-		iRenderer.renderInline(parent, image);
-	}
+//	public void imageRenderer(HtmlElement parent, Image image)
+//	{
+//		HtmlImageRenderer iRenderer = new HtmlImageRenderer(cmm, dsm);
+//		iRenderer.renderInline(parent, image);
+//	}
 }
