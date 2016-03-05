4da119862877ae7ffbb901e6321b562076e2f813
Rebecca Roblick <tandora@gmx.net>
2016-03-05 14:15:38 +0100

    Comments for IHK temporarily added

--- core/src/test/java/org/openfuxml/renderer/html/structure/TestHtmlHeadingRenderer.java
+++ core/src/test/java/org/openfuxml/renderer/html/structure/TestHtmlHeadingRenderer.java
@@ -21,20 +21,31 @@ public class TestHtmlHeadingRenderer extends AbstractTestHtmlRenderer
 	
 	private HtmlBody renderer;
 
+	/*Initialisierung des Zielordners und des Renderers.
+	* @Before JUnit Test Annotation, sorgt dafür dass diese Methode immer zuerst aufgerufen wird.
+	* Wichtig wenn man dies als JUnit Test ausführt und nicht als Applikation über die
+	* main() Methode.*/
 	@Before public void init()
 	{
 		super.initDir("section");
 		renderer = new HtmlBody(new NoOpCrossMediaManager(), new OfxDefaultSettingsManager());
 	}
 
+	/*initFile(arg) initalisiert die Ausgabedatei @para arg: Dateiname
+	* Konvertierung der Dummy “Section”, enthält die zu testende Überschrift.
+	* renterTest(arg) speichert Ausgabedatei und vergleicht String-output mit Dateiinhalt als
+	* JUnit Test
+	* @Test: JUnit Annotation, ählich der @Before Annotation für JUnit Tests.
+	* Deklariert die eigentlichen Tests.
+	*/
 	@Test public void lvl1() throws IOException
 	{
 		initFile(Key.lvl1);
-
         renderer.render(SectionProvider.build());
     	renderTest(renderer);
 	}
-	
+
+	//Ähnlich lvl1(), hier wird nur zusätzlich eine untergeordnete Überschrift erzeugt.
 	@Test public void lvl2() throws IOException
 	{
 		initFile(Key.lvl2);
@@ -43,6 +54,7 @@ public class TestHtmlHeadingRenderer extends AbstractTestHtmlRenderer
     	renderTest(renderer);
 	}
 
+	//main() zum Ausführen als Applikation, wichtig vor jedem einzelnen Test init() aufzurufen.
 	public static void main(String[] args) throws IOException
 	{
 		OfxCoreTestBootstrap.init();
