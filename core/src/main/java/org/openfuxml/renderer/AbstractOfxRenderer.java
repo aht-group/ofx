878dc88e09f8d17c71244ec5f49e1f3ce89c7ad4
Thorsten <t.kisner@web.de>
2016-03-05 08:14:31 +0700

    ConfigurationProviderFactory added

--- core/src/main/java/org/openfuxml/renderer/AbstractOfxRenderer.java
+++ core/src/main/java/org/openfuxml/renderer/AbstractOfxRenderer.java
@@ -1,5 +1,7 @@
 package org.openfuxml.renderer;
 
+import org.openfuxml.factory.ConfigurationProviderFacotry;
+import org.openfuxml.interfaces.ConfigurationProvider;
 import org.openfuxml.interfaces.DefaultSettingsManager;
 import org.openfuxml.interfaces.media.CrossMediaManager;
 import org.openfuxml.renderer.latex.content.structure.LatexSectionRenderer;
@@ -10,11 +12,20 @@ public class AbstractOfxRenderer
 {
 	final static Logger logger = LoggerFactory.getLogger(LatexSectionRenderer.class);
 	
+	protected ConfigurationProvider cp;
+	
 	protected CrossMediaManager cmm;
 	protected DefaultSettingsManager dsm;
 	
+	public AbstractOfxRenderer(ConfigurationProvider cp)
+	{
+		this.cp=cp;
+	}
+	
+	@Deprecated //Use ConfigurationProvider
 	public AbstractOfxRenderer(CrossMediaManager cmm, DefaultSettingsManager dsm)
 	{
+		cp = ConfigurationProviderFacotry.build(cmm,dsm);
 		this.cmm=cmm;
 		this.dsm=dsm;
 	}
