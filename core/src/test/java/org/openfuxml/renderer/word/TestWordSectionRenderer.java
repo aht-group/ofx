ffd435a7d1a577bf1cac28b3f53659bf2da8f1a9
Thane89 <krueger@aht-group.com>
2016-05-18 08:58:14 +0200

    bugfix

--- core/src/test/java/org/openfuxml/renderer/word/TestWordSectionRenderer.java
+++ core/src/test/java/org/openfuxml/renderer/word/TestWordSectionRenderer.java
@@ -1,12 +1,11 @@
 package org.openfuxml.renderer.word;
 
 import org.openfuxml.content.ofx.Section;
+import org.openfuxml.factory.ConfigurationProviderFacotry;
 import org.openfuxml.interfaces.configuration.ConfigurationProvider;
-import org.openfuxml.interfaces.configuration.DefaultSettingsManager;
-import org.openfuxml.interfaces.media.CrossMediaManager;
+import org.openfuxml.media.cross.NoOpCrossMediaManager;
 import org.openfuxml.renderer.word.structure.WordSectionRenderer;
-
-import com.sun.media.jfxmedia.logging.Logger;
+import org.openfuxml.util.configuration.settings.OfxDefaultSettingsManager;
 
 import net.sf.exlp.util.xml.JaxbUtil;
 
@@ -16,20 +15,12 @@ public static void main(String[] args) throws Exception{
 	int lvl = 0;
 	
 	TestWordSectionRenderer twsr = new TestWordSectionRenderer();
-	WordSectionRenderer wsr = new WordSectionRenderer(new ConfigurationProvider() {
-		
-		@Override
-		public DefaultSettingsManager getDefaultSettingsManager() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		@Override
-		public CrossMediaManager getCrossMediaManager() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	}, lvl);	
+	
+	OfxDefaultSettingsManager dsm = new OfxDefaultSettingsManager();
+	NoOpCrossMediaManager cmm = new NoOpCrossMediaManager();
+	ConfigurationProvider cp = ConfigurationProviderFacotry.build(cmm, dsm);
+	
+	WordSectionRenderer wsr = new WordSectionRenderer(cp, lvl);	
 	
 	Section s = twsr.buildSection(); 
 	
