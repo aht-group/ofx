a21d09548fc2d49b31865cfffb957e7ed3e0fdcc
Thorsten-K <t.kisner@web.de>
2017-06-08 10:57:25 +0200

    Svg2SvgTranscode now can write files

--- core/src/main/java/org/openfuxml/media/transcode/Svg2SvgTranscoder.java
+++ core/src/main/java/org/openfuxml/media/transcode/Svg2SvgTranscoder.java
@@ -1,12 +1,14 @@
 package org.openfuxml.media.transcode;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.transcoder.TranscoderException;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,5 +23,10 @@ public class Svg2SvgTranscoder
 	    Writer out = new OutputStreamWriter(os, "UTF-8");
 	    g.stream(out, useCSS);
 	    return os.toByteArray();
+	}
+	
+	public static void transcode(SVGGraphics2D g, File f) throws IOException, TranscoderException
+	{
+		FileUtils.writeByteArrayToFile(f, transcode(g));
 	}
 }
