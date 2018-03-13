1eb7aa56707ced3481639c7772a82b195e7bb989
Thorsten-K <t.kisner@web.de>
2018-03-13 13:41:34 +0100

    SectionXPath extended

--- xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
+++ xml/src/main/java/org/openfuxml/xml/xpath/content/SectionXpath.java
@@ -1,16 +1,18 @@
 package org.openfuxml.xml.xpath.content;
 
+import java.io.Serializable;
 import java.util.List;
-
-import net.sf.exlp.exception.ExlpXpathNotFoundException;
-import net.sf.exlp.exception.ExlpXpathNotUniqueException;
 
 import org.apache.commons.jxpath.JXPathContext;
 import org.openfuxml.content.ofx.Listing;
 import org.openfuxml.content.ofx.Section;
+import org.openfuxml.content.ofx.Sections;
 import org.openfuxml.content.ofx.Title;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.sf.exlp.exception.ExlpXpathNotFoundException;
+import net.sf.exlp.exception.ExlpXpathNotUniqueException;
 
 public class SectionXpath
 {
@@ -30,21 +32,21 @@ public class SectionXpath
 
     public static synchronized Section getRenderer(Section section, String id) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
     {
-    	Section result = null;
-    	for(Object o : section.getContent())
-    	{
-    		if(o instanceof Section)
-    		{
-    			Section child = (Section)o;
-    			if(child.getId().equals(id))
-    			{
-    				if(result!=null){throw new ExlpXpathNotUniqueException("No unique "+Section.class.getSimpleName()+" for id="+id);}
-    				result = child;
-    			}
-    		}
-    	}
-    	if(result==null){throw new ExlpXpathNotFoundException("No "+Section.class.getSimpleName()+" found for id="+id);}
-    	return result;
+	    	Section result = null;
+	    	for(Object o : section.getContent())
+	    	{
+	    		if(o instanceof Section)
+	    		{
+	    			Section child = (Section)o;
+	    			if(child.getId().equals(id))
+	    			{
+	    				if(result!=null){throw new ExlpXpathNotUniqueException("No unique "+Section.class.getSimpleName()+" for id="+id);}
+	    				result = child;
+	    			}
+	    		}
+	    	}
+	    	if(result==null){throw new ExlpXpathNotFoundException("No "+Section.class.getSimpleName()+" found for id="+id);}
+	    	return result;
     }
     
     public static synchronized Listing getSeed(Section section, String id) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
@@ -60,4 +62,24 @@ public class SectionXpath
 		else if(listResult.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Listing.class.getSimpleName()+" for id="+id);}
 		return listResult.get(0);
 	}
+    
+    public static <E extends Enum<E>> Section sectionForUniqueClassifier(Sections sections, E classifier) throws ExlpXpathNotUniqueException, ExlpXpathNotFoundException
+    {
+    		Section section = null;
+    		boolean found = false;
+    		for(Serializable s  : sections.getContent())
+    		{
+    			if(s instanceof Section)
+    			{
+    				section = (Section)s;
+    				if(section.getClassifier().contentEquals(classifier.toString()))
+    				{
+    					if(found==true) {throw new ExlpXpathNotUniqueException("Section not unique");}
+    					found=true;
+    				}
+    			}
+    		}
+    		if(found==false) {throw new ExlpXpathNotFoundException("Section not available");}
+    		return section;
+    }
 }
