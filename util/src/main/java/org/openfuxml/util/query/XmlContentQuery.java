afcd539a55906083a05ee8cb1b576ac52d476711
Petersen <petersen@aht-group.com>
2018-08-22 15:19:45 +0200

    add XmlContentQuery

---
+++ util/src/main/java/org/openfuxml/util/query/XmlContentQuery.java
@@ -0,0 +1,28 @@
+package org.openfuxml.util.query;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.openfuxml.content.list.Item;
+import org.openfuxml.content.ofx.Paragraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XmlContentQuery
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlContentQuery.class);
+	
+	public static boolean hasMore(Object o, List<Serializable> list)
+	{
+		int index = list.indexOf(o)+1;
+		boolean hasMore = false;
+		for(Object x : list.subList(index, list.size()-1))
+		{
+			if(x instanceof Paragraph) {hasMore=true;}
+			if(x instanceof Item) {hasMore=true;}
+		}
+		return hasMore;
+	}
+	
+	
+}
