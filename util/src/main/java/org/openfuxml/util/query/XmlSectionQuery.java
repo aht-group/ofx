0c6f03b2ce9d670761a7074056f7b5d5914c7992
Thorsten-K <t.kisner@web.de>
2020-02-18 08:06:19 +0100

    Exception added to XmlSectionQuery

--- util/src/main/java/org/openfuxml/util/query/XmlSectionQuery.java
+++ util/src/main/java/org/openfuxml/util/query/XmlSectionQuery.java
@@ -5,16 +5,17 @@ import java.io.Serializable;
 import org.openfuxml.content.ofx.Section;
 import org.openfuxml.content.ofx.Sections;
 
+import net.sf.exlp.exception.ExlpXpathNotFoundException;
+
 public class XmlSectionQuery
 {
-	public static <E extends Enum<E>> Section find(Sections sections, E classifier)
+	public static <E extends Enum<E>> Section find(Sections sections, E classifier) throws ExlpXpathNotFoundException
 	{
 		return find(sections,classifier.toString());
 	}
 	
-	private static Section find(Sections sections, String classifier)
+	private static Section find(Sections sections, String classifier) throws ExlpXpathNotFoundException
 	{
-		if(sections==null) {return null;}
 		for(Serializable s : sections.getContent())
 		{
 			if(s instanceof Section)
@@ -23,6 +24,6 @@ public class XmlSectionQuery
 				if(section.isSetClassifier() && section.getClassifier().equals(classifier)){return section;}
 			}
 		}
-		return null;
+		throw new ExlpXpathNotFoundException("No section found for classifier="+classifier) ;
 	}
 }
