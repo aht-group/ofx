514838f25e5dddd2c14c2b4c8e7377bff7ecb7b1
Lasse Petersen <petersen@aht-group.com>
2021-05-01 15:18:12 +0200

    OfxWordFont add some defaults

--- util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordFont.java
+++ util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordFont.java
@@ -1,39 +1,63 @@
 package org.openfuxml.util.configuration.settings.word;
 
+import java.awt.Color;
+
 public class OfxWordFont
 {
 	private boolean italic,italicDefault; public boolean isItalic() {return italic;}
 	private boolean bold,boldDefault; public boolean isBold() {return bold;}
-	// ... underline
-	
-	// ... size
+	private boolean underline,underlineDefault; public boolean isUnderline() {return bold;}
+	private double size, sizeDefault; public double getSize() {return size;}
+	private Color color, colorDefault; public Color getColor() {return color;}
+	private double ident, identDefault; public double getIdent() {return ident;}
 	
 	public static OfxWordFont instance() {return new OfxWordFont();}
 	
 	public OfxWordFont()
 	{
-		italic = false;
+		size = 10;
 		bold = false;
-		
+		italic = false;
+		underline = false;
+		color = Color.BLACK;
+		ident = 0;
 		save();
 	}
 	
 	public void save()
 	{
-		italicDefault = italic;
+		sizeDefault = size;
 		boldDefault = bold;
+		italicDefault = italic;
+		underlineDefault = underline;
+		colorDefault = color;
+		identDefault = ident;
 	}
 	
 	public OfxWordFont reset()
 	{
-		italic = italicDefault;
+		size = sizeDefault;
 		bold = boldDefault;
+		italic = italicDefault;
+		underline = underlineDefault;
+		color = colorDefault;
+		ident = identDefault;
+		
 		return this;
 	}
+	
+	public OfxWordFont size(double value) {size=value;return this;}
 	
 	public OfxWordFont italic() {italic(true);return this;}
 	public OfxWordFont italic(boolean value) {italic=value;return this;}
 	
 	public OfxWordFont bold() {bold(true);return this;}
 	public OfxWordFont bold(boolean value) {bold=value;return this;}
+	
+	public OfxWordFont underline() {underline(true);return this;}
+	public OfxWordFont underline(boolean value) {underline=value;return this;}
+	
+	public OfxWordFont color(Color value) {color=value;return this;}
+	
+	public OfxWordFont ident(double value) {ident=value;return this;}
 }
