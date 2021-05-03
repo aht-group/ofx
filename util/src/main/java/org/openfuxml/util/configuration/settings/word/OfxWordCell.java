fa7bda7f468a2f3f9165110183a2958a3ac77660
Lasse Petersen <petersen@aht-group.com>
2021-05-03 10:11:11 +0200

    OfxWordCell all needed defaults (all identified so far) added

--- util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordCell.java
+++ util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordCell.java
@@ -6,26 +6,26 @@ public class OfxWordCell
 {
 	private	int paragraphAlignment; public int getParagraphAlignment() { return paragraphAlignment; }
 	private	int verticalAlignment; public int getVerticalAlignment() { return verticalAlignment; }
-	private	double cWith; public double getcWith() { return cWith; }
+	private	double cellWidth; public double getCellWith() { return cellWidth; }
 	
 	private	int topLineStyle; public int getTopLineStyle() { return topLineStyle; }
 	private	double topLineWidth; public double getTopLineWidth() { return topLineWidth; 	}
 	private	Color topLineBorderColor; public Color getTopLineBorderColor() { return topLineBorderColor; }
+
+	private	int rightLineStyle; public int getRightLineStyle() { return rightLineStyle; }
+	private	double rightLineWidth; public double getRightLineWidth() { return rightLineWidth; 	}
+	private	Color rightLineBorderColor; public Color getRightLineBorderColor() { return rightLineBorderColor; }
 	
-//	private	int rightLineStyle;
-//	private	double rightLineWidth;
-//	private	Color rightLineBorderColor;
+	private	int bottomLineStyle; public int getBottomLineStyle() { return bottomLineStyle; }
+	private	double bottomLineWidth; public double getBottomLineWidth() { return bottomLineWidth; 	}
+	private	Color bottomLineBorderColor; public Color getBottomLineBorderColor() { return bottomLineBorderColor; }
 	
-//	private	int bottomLineStyle;
-//	private	double bottomLineWidth;
-//	private	Color bottomLineBorderColor;
+	private	int leftLineStyle; public int getLeftLineStyle() { return leftLineStyle; }
+	private	double leftLineWidth; public double getLeftLineWidth() { return leftLineWidth; 	}
+	private	Color leftLineBorderColor; public Color getLeftLineBorderColor() { return leftLineBorderColor; }
 	
-//	private	int leftLineStyle;
-//	private	double leftLineWidth;
-//	private	Color leftLineBorderColor;
-	
-//	private Color tableBackgroundColor;
-//	private int textureIndex;
+	private Color tableBackgroundColor; public Color getTablBackgroundColor() { return tableBackgroundColor;}
+	private int textureIndex; public int getTextureIndex() {return textureIndex;}
 	
 	public static OfxWordCell instance() {return new OfxWordCell();}
 	
@@ -33,9 +33,58 @@ public class OfxWordCell
 	{
 		paragraphAlignment = 0;
 		verticalAlignment = 0;
-		cWith = 100;
+		cellWidth = 100;
+		
+		topLineStyle = 0;
+		topLineWidth = 1;
+		topLineBorderColor = Color.BLACK;
+		
+		leftLineStyle = 0;
+		leftLineWidth = 1;
+		leftLineBorderColor = Color.BLACK;
+		
+		bottomLineStyle = 0;
+		bottomLineWidth = 1;
+		bottomLineBorderColor = Color.BLACK;
+		
+		rightLineStyle = 0;
+		rightLineWidth = 1;
+		rightLineBorderColor = Color.BLACK;
+		
+		tableBackgroundColor = Color.WHITE;
+		textureIndex = 0;
 	}
 	
+	public OfxWordCell paragraphAlignmentCenter() {paragraphAlignment(1);return this;}
+	public OfxWordCell paragraphAlignmentRight() {paragraphAlignment(2);return this;}
 	public OfxWordCell paragraphAlignment(int value) {paragraphAlignment=value;return this;}
+
+	public OfxWordCell verticalAlignmentCenter() {verticalAlignment(1);return this;}
+	public OfxWordCell verticalAlignmentBottom() {verticalAlignment(2);return this;}
 	public OfxWordCell verticalAlignment(int value) {verticalAlignment=value;return this;}
+	
+	public OfxWordCell cellWith(double value) {cellWidth=value;return this;}
+	
+	public OfxWordCell topLineStyleSingle() {topLineStyle(1);return this;}
+	public OfxWordCell topLineStyle(int value) {topLineStyle=value;return this;}
+	public OfxWordCell topLineWidth(double value) {topLineWidth=value;return this;}
+	public OfxWordCell topLineBorderColor(Color value) {topLineBorderColor=value;return this;}
+
+	public OfxWordCell rightLineStyleSingle() {rightLineStyle(1);return this;}
+	public OfxWordCell rightLineStyle(int value) {rightLineStyle=value;return this;}
+	public OfxWordCell rightLineWidth(double value) {rightLineWidth=value;return this;}
+	public OfxWordCell rightLineBorderColor(Color value) {rightLineBorderColor=value;return this;}
+	
+	public OfxWordCell bottomLineStyleSingle() {bottomLineStyle(1);return this;}
+	public OfxWordCell bottomLineStyle(int value) {bottomLineStyle=value;return this;}
+	public OfxWordCell bottomLineWidth(double value) {bottomLineWidth=value;return this;}
+	public OfxWordCell bottomLineBorderColor(Color value) {bottomLineBorderColor=value;return this;}
+
+	public OfxWordCell leftLineStyleSingle() {leftLineStyle(1);return this;}
+	public OfxWordCell leftLineStyle(int value) {leftLineStyle=value;return this;}
+	public OfxWordCell leftLineWidth(double value) {leftLineWidth=value;return this;}
+	public OfxWordCell leftLineBorderColor(Color value) {leftLineBorderColor=value;return this;}
+	
+	public OfxWordCell tableBackgroundColor(Color value) {tableBackgroundColor=value;return this;}
+	public OfxWordCell textureIndex(int value) {textureIndex=value;return this;}
 }
