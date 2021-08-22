8c78c62716f80d7cb2fa93b0099016fd4be2565f
Thorsten-K <t.kisner@web.de>
2021-08-22 13:59:57 +0200

    formatting improved

--- util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordCell.java
+++ util/src/main/java/org/openfuxml/util/configuration/settings/word/OfxWordCell.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 
 import com.aspose.words.CellVerticalAlignment;
 import com.aspose.words.LineStyle;
-import com.aspose.words.ParagraphAlignment;
 
 public class OfxWordCell
 {
