6fa922245c08d4c08c33e22c6a4d46a9b5981ad6
Thorsten-K <t.kisner@web.de>
2024-03-16 21:50:47 +0200

    test fox ofx-xml tests

--- wiki/src/test/java/org/openfuxml/addon/wiki/processor/markup/TestWikiInlineProcessor.java
+++ wiki/src/test/java/org/openfuxml/addon/wiki/processor/markup/TestWikiInlineProcessor.java
@@ -18,6 +18,7 @@ import org.openfuxml.exception.OfxConfigurationException;
 import org.openfuxml.exception.OfxInternalProcessingException;
 import org.openfuxml.model.xml.core.ofx.Section;
 import org.openfuxml.test.AbstractFileProcessingTest;
+import org.openfuxml.test.OfxBootstrap;
 import org.openfuxml.xml.OfxNsPrefixMapper;
 import org.openfuxml.xml.renderer.cmp.Cmp;
 import org.slf4j.Logger;
@@ -90,9 +91,7 @@ public class TestWikiInlineProcessor extends AbstractFileProcessingTest
 	
 	public static void main(String[] args) throws FileNotFoundException, OfxConfigurationException, OfxInternalProcessingException
     {
-		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
-			loggerInit.path("src/test/resources/config");
-			loggerInit.init();	
+		OfxBootstrap.init();
 		JaxbUtil.setNsPrefixMapper(new OfxNsPrefixMapper());
 			
 		boolean saveReference = true;
